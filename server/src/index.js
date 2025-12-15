cb321178812431631c137647ca33f7667d919af0
vanii0002 <0002sharbansal@gmail.com>
2025-12-15 22:06:38 +0530

    .env test

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,35 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import router from './routes/UserRouter.js';
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb://localhost:27017/Cod_Tourment', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to MongoDB');
+}
+).catch(err => {
+    console.error('Error connecting to MongoDB', err);
+}
+);
+
+
+
+dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.get('/', (req, res) => {
+
+
+app.use(express.json());
+
+
+
+app.get('/api', (req, res) => {
     res.send('Hello World!');
 });
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
