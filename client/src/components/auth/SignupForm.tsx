7ef44748eaa5762553857706629275ea8824353c
sam.megh0305@gmail.com <sam.megh0305@gmail.com>
2026-04-29 21:31:02 +0530

    new dev

--- client/src/components/auth/SignupForm.tsx
+++ client/src/components/auth/SignupForm.tsx
@@ -5,7 +5,7 @@ import PhoneInput from "react-phone-input-2";
 import "react-phone-input-2/lib/style.css";
 import AuthInput from "./AuthInput";
 import RoleSelect from "./RoleSelect";
-import type { SignupPayload,UserRole } from "../../interface/auth.types";
+import type { SignupPayload,UserRole } from "../../Interface/auth.types";
 import GoogleAuthButton from "./GoogleAuthButton";
 import PendingGoogleForm from "../../components/auth/PendingGoogleForm";
 import { useSignup,useGoogleAuth } from "../../files/CheckAuthFile";
--- client/src/files/CheckAuthFile.ts
+++ client/src/files/CheckAuthFile.ts
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { toast } from "react-toastify";
 import instance from "../lib/Axios";
-import type { SignupPayload,LoginPayload,UserRole } from "../interface/auth.types";
+import type { SignupPayload,LoginPayload,UserRole } from "../Interface/auth.types";
 
 type GoogleResponse = {
   credential: string;
